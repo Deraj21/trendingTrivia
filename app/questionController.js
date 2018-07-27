dc83f33045e4acfacb09137a08475f7519b18cec
deraj21 <deraj21@gmail.com>
2018-07-26 17:55:43 -0700

    can now post new question

--- app/questionController.js
+++ app/questionController.js
@@ -5,42 +5,84 @@ angular.module('myApp')
     animal: '',
     correct_answer: 1,
     difficulty: 1,
-    options: {}
+    options: {
+      1: '',
+      2: '',
+      3: '',
+      4: ''
+    }
   };
   $scope.searchFilter = 'filter';
   
+  $scope.getQuestions = function(){
+    questionService.getQuestions()
+      .then(response => {
+        $scope.questions = response.data;
+      });
+  }
+  $scope.getQuestions();
+  
+  // reGETs the questions base on difficulty
   $scope.filterByDifficulty = function(diff){
     if (diff === '') {
       questionService.getQuestions()
-        .then(response => {
-          $scope.questions = response.data;
-        });
+      .then(response => {
+        $scope.questions = response.data;
+      });
     } else {
       questionService.getFilteredQuestions(diff)
-        .then(response => {
-          $scope.questions = response.data;
-        });
+      .then(response => {
+        $scope.questions = response.data;
+      });
     }
   };
   
+  // turns integer into difficulty text
   $scope.difficultyText = function(diff) {
     switch (diff) {
       case 1:
         return 'Easy';
-      case 2:
+        case 2:
         return 'Medium';
-      case 3:
+        case 3:
         return 'Hard';
-      default:
+        default:
         return '';
-    }
+      }
   };
+  
+  // hides and shows the 'Add Question' html
+  $scope.hideModal = function(){
+    let modal = document.querySelector(".modal");
+    modal.setAttribute('style', "display: none;");
+  };
+  $scope.showModal = function(){
+    let modal = document.querySelector('.modal');
+    modal.setAttribute('style', 'display: flex;');
+  }
+  
+  // sends new question up to the cloud
+  $scope.saveNewQuestion = function(){
+    let { one, two, three, four } = $scope;
+    if (one) { $scope.newQuestion.correct_answer = 1; }
+    else if (two) { $scope.newQuestion.correct_answer = 2; }
+    else if (three) { $scope.newQuestion.correct_answer = 3; }
+    else if (four) { $scope.newQuestion.correct_answer = 4; }
+    else { console.log('no correct answer'); }
 
-  questionService.getQuestions()
-    .then(response => {
-      $scope.questions = response.data;
-    });
-
+    console.log('saving new question:', $scope.newQuestion);
+    questionService.postNewQuestion($scope.newQuestion)
+      .then( response => {
+        $scope.questions = response.data;
+      });
+    $scope.hideModal();
+  }
+  // 
+  $scope.cancelNewQuestion = function(){
+    console.log('cancel new question');
+    $scope.hideModal();
+  }
+  
 });
 
 /**
--- app/questionService.js
+++ app/questionService.js
@@ -2,4 +2,5 @@ angular.module('myApp')
 .service('questionService', function($http) {
   this.getQuestions = () => $http.get(`https://practiceapi.devmountain.com/api/trivia/questions`);
   this.getFilteredQuestions = difficulty => $http.get(`https://practiceapi.devmountain.com/api/trivia/questions/difficulty/${difficulty}`);
+  this.postNewQuestion = newQuestion => $http.post(`https://practiceapi.devmountain.com/api/trivia/questions`, newQuestion);
 });
